d35dcdf046456783898a0c149d2ad979e820f07f
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-10-04 14:53:27 -0300

    feat: install and setup eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import pluginJs from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
+  { ignores: ['dist/', 'swagger'] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          trailingComma: 'all',
+          arrowParens: 'always',
+          semi: false,
+        },
+      ],
+    },
+  },
+]
