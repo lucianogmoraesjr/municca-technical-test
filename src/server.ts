5107763414ea46d2fef3002978b1bc930ce17e42
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-10-04 14:43:01 -0300

    feat: install and setup express

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (request, response) => {
+  response.json({message: 'hello world'})
+})
+
+app.listen(3333, () => console.log('🔥 server running on http://localhost:3333'))
